35be8372321d93142c9e6b9bff32eecd18189314
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2025-02-05 19:41:14 +0530

    schema for transactions

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,20 @@
-import { integer, pgTable } from "drizzle-orm/pg-core";
+import { integer, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const spendingType = pgEnum("spending_type", ["spent", "earned"]);
+export const transactionType = pgEnum("transaction_type", [
+  "food",
+  "shopping",
+  "transport",
+  "friends",
+  "other",
+]);
 
 export const transactions = pgTable("transactions", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  amount: integer().notNull(),
+  spending_type: spendingType().notNull(),
+  created_at: timestamp({ withTimezone: true }).defaultNow().notNull(),
+  updated_at: timestamp({ withTimezone: true }),
+  description: text().notNull(),
+  transaction_type: transactionType().notNull(),
 });
