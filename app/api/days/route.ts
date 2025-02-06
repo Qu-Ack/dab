0312880c611a763741e4626d5897befc53480f23
Qu-Ack <134773611+Qu-Ack@users.noreply.github.com>
2025-02-06 15:55:46 +0530

    api days and vercel.json test

--- app/api/days/route.ts
+++ app/api/days/route.ts
@@ -2,7 +2,13 @@ import db from "@/db";
 import { days } from "@/db/schema";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: Request) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return NextResponse.json({ status: "unauthorized" });
+  }
+
   await db.insert(days).values({});
 
   return NextResponse.json({ status: "ok" });
